80cd8578fb780350b6f9f68a3914b3afe93e3339
Weston Farmer <wesfarmer11@gmail.com>
2024-04-05 16:46:06 -0500

    included buttons for drum pad class elements

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ function App() {
       <header className="App-header">
         Drum Machine
       </header>
+
+      <div id="drum-machine">
+        <div id="display">
+          <button className="drum-pad" id="Heater1">Q</button>
+          <button className="drum-pad" id="Heater2">W</button>
+          <button className="drum-pad" id="Heater3">E</button>
+          <button className="drum-pad" id="Heater4">A</button>
+          <button className="drum-pad" id="Clap">S</button>
+          <button className="drum-pad" id="Open-HH">D</button>
+          <button className="drum-pad" id="Kick-n-Hat">Z</button>
+          <button className="drum-pad" id="Kick">X</button>
+          <button className="drum-pad" id="closed-HH">C</button>
+
+        </div>
+      </div>
     </div>
   );
 }
